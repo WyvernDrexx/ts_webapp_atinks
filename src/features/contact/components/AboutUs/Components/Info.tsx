bcffb4fd19f06216317de9c3a700d47a1bcba444
LPSharma <lilaprashadsharma@gmail.com>
2023-03-12 21:33:23 +0530

    contact us card initial design

--- src/features/contact/components/AboutUs/Components/Info.tsx
+++ src/features/contact/components/AboutUs/Components/Info.tsx
@@ -3,16 +3,63 @@ import {
   faLocationPin,
   faClock,
   faLink,
-  faQuoteLeft
+  faQuoteLeft,
+  faEnvelope,
+  faPhone
 } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { cn } from 'utils'
 import { Card } from './Card'
 
 function Info() {
+  const emails = ['salesteam@br.in', 'salesteam2@br.in', 'salesteam3@br.in']
+  const phoneNumbers = ['+91 8511591740', '8000058214', '112233445566']
+
   const cards = [
     {
       title: 'Contact',
       icon: faAddressBook,
-      onEditClick: () => alert('Clicked!')
+      onEditClick: () => alert('Clicked!'),
+      Body: (
+        <div className='ml-[2px] mt-10'>
+          <div className='flex items-center relative'>
+            <FontAwesomeIcon color='#c3c3c3' icon={faEnvelope} />
+            <div className='relative flex w-[100%]'>
+              <p className='ml-2'>{emails.slice(0, 2).join(' / ')}</p>
+              {emails.length > 2 && (
+                <span
+                  className={cn(
+                    'text-red-500 bg-[#fef3f3]',
+                    'absolute right-0 bottom-0 h-8 w-8',
+                    'text-xs ',
+                    'flex justify-center items-center rounded-full'
+                  )}
+                >
+                  +{emails.length - 2}
+                </span>
+              )}
+            </div>
+          </div>
+          <div className='flex items-center mt-4 relative'>
+            <FontAwesomeIcon color='#c3c3c3' icon={faPhone} />
+            <div className='relative flex w-[100%]'>
+              <p className='ml-2'>{phoneNumbers.slice(0, 2).join(' / ')}</p>
+              {phoneNumbers.length > 2 && (
+                <span
+                  className={cn(
+                    'text-red-500 bg-[#fef3f3]',
+                    'absolute right-0 bottom-0 h-8 w-8',
+                    'text-xs ',
+                    'flex justify-center items-center rounded-full'
+                  )}
+                >
+                  +{phoneNumbers.length - 2}
+                </span>
+              )}
+            </div>
+          </div>
+        </div>
+      )
     },
     {
       title: 'Address',
@@ -39,7 +86,9 @@ function Info() {
   return (
     <div className='grid grid-cols-3 gap-5'>
       {cards.map((e) => (
-        <Card icon={e.icon} onEditClick={e.onEditClick} title={e.title} />
+        <Card icon={e.icon} onEditClick={e.onEditClick} title={e.title}>
+          {e.Body && e.Body}
+        </Card>
       ))}
     </div>
   )
