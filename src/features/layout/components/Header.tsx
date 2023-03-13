52fd21780af4b52c84a24ad15ecf860d255b7c09
LPSharma <lilaprashadsharma@gmail.com>
2023-03-13 15:03:22 +0530

    header search input design

--- src/features/layout/components/Header.tsx
+++ src/features/layout/components/Header.tsx
@@ -1,5 +1,5 @@
-import { TextField } from '@mui/material'
-import React from 'react'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { cn } from 'utils'
 
 function Header() {
@@ -7,20 +7,30 @@ function Header() {
     <div
       className={cn(
         'pl-8',
-        'h-[100px]',
+        'h-[65px]',
         'flex items-center',
         'grid',
         'grid-cols-[300px_minmax(900px,_1fr)]'
       )}
     >
       <div>
-        <img width={120} src='logo.png' />
+        <img className='h-[50px]' src='logo.png' />
       </div>
       <div className='ml-8'>
-        <TextField placeholder='Search' className={cn('w-[589px]')} />
+        <div className={cn('w-[589px] pl-3 rounded-xl flex items-center', 'bg-[#eff2f9]')}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} />
+          <input
+            type='text'
+            id='first_name'
+            className='ml-3 bg-[#eff2f9] text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  '
+            placeholder='Search'
+            required
+          ></input>
+        </div>
       </div>
     </div>
   )
 }
 
 export { Header }
+
